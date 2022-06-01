83a4c279e242929ca81eb0b6aad7369aad385269
Jonas Lederer <jonas.lederer@tu-berlin.de>
2022-06-01 14:13:39 +0200

    removed redundant package imports

--- src/schnetpack/cli.py
+++ src/schnetpack/cli.py
@@ -16,8 +16,6 @@ from schnetpack.utils.script import log_hyperparameters, print_config
 from schnetpack.data import BaseAtomsData, AtomsLoader
 from schnetpack.train import PredictionWriter
 
-import tempfile
-
 log = logging.getLogger(__name__)
 
 
